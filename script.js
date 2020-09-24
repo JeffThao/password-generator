5eda48aa9d2515500bde304e55fb20d2e9f69552
Jeff T <jeff.thao92@gmail.com>
2020-09-24 15:57:15 -0500

    Added a while loop and added variables and an array for the characters for the password.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var passwordLength;
+
+var passwordChar = {
+  number: "0123456789",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  specialChar: "~!@#$%&-+=_"
+};
+
+
+//Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  passwordLength = prompt("How long would you like your password to be? (8-128 characters)");
+
+  while ((passwordLength == null) || (passwordLength < 8) || (passwordLength > 128)) {
+
+    prompt("Please choose a password length between 8 to 128 characters.");
+
+    var password = generatePassword(); var passwordText = document.querySelector("#password");
+
+  }
+
 
   passwordText.value = password;
 
