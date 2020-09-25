773152fea58a4cafc095e34747685e904ab03cdc
Jeff Thao <jeff.thao92@gmail.com>
2020-09-24 22:24:25 -0500

    added if statements to remove an array if the user didn't want it. Also added for statement to generate password, not working yet, may need to add another variable to call out.

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
 var generateBtn = document.querySelector("#generate");
-
 var passwordLength;
-
+var confirmNum;
+var confirmLower;
+var confirmUpper;
+var confirmSpecial;
 var passwordChar = {
-  number: "0123456789",
+  num: "0123456789",
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   specialChar: "~!@#$%&-+=_"
 };
 
-
 //Write password to the #password input
 function writePassword() {
-  passwordLength = prompt("How long would you like your password to be? (8-128 characters)");
 
-  while ((passwordLength == null) || (passwordLength < 8) || (passwordLength > 128)) {
+  var passwordLength = prompt("How long would you like your password to be? (8-128 characters)");
 
-    prompt("Please choose a password length between 8 to 128 characters.");
+  while ((passwordLength === null) || (passwordLength < 8) || (passwordLength > 128)) {
+    passwordLength = prompt("Please choose a password length between 8 to 128 characters.");
+    console.log(passwordLength);
 
-    var password = generatePassword(); var passwordText = document.querySelector("#password");
+    if ((passwordLength > 8) || (passwordLength < 128 )){
+      console.log(passwordLength)
+      confirmNum = confirm("Do you want numbers?");
+      confirmLower = confirm("Do you want lowercase letters?");
+      confirmUpper = confirm("Do you want uppercase letters?");
+      confirmSpecial = confirm("Do you want special characters? (~!@#$%&-+=_)");
+    }
+
+    if (confirmNum !== true) {
+      // numbers = "";
+      passwordChar = remove(passwordChar.num);
+    }
+
+    if (confirmLower !== true) {
+      passwordChar = remove(passwordChar.lowerCase);
+    }
+
+    if (confirmUpper !== true) {
+      passwordChar = remove(passwordChar.upperCase);
+    }
+
+    if (confirmSpecial !== true) {
+      passwordChar = remove(passwordChar.specialChar);
+    }
+
+    if ((confirmNum !== true) && (confirmLower !== true) && (confirmUpper !== true) && (confirmSpecial !== true)) {
+      prompt("Please select AT LEAST two of the criterias.");
+    }
 
   }
 
+    for (var i = 0; i < passwordLength; i++) {
+      var word = Math.floor(Math.random()*passwordChar.length);
+      
+    }
 
-  passwordText.value = password;
+   passwordText.value = password;
 
 }
 
