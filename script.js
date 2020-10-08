70f348b648e817dd2d4f1cb656853101828a99a1
Jeff T <jeff.thao92@gmail.com>
2020-10-08 16:34:00 -0500

    corrected some spacing issues.

--- script.js
+++ script.js
@@ -17,15 +17,16 @@ function writePassword() {
   while ((passwordLength == null) || (passwordLength < 8) || (passwordLength > 128)) {
 
     prompt("Please choose a password length between 8 to 128 characters.");
-
-    var password = generatePassword(); var passwordText = document.querySelector("#password");
-
   }
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 
-
-  passwordText.value = password;
-
+  
 }
+ 
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
